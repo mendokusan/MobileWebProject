3fcbcd10b876c95eb6d9d6dacaf131ffcd8b663d
Ella Dixon <elladixon@elladixon-macbookair.local>
2020-10-28 10:14:03 +1300

    foodbank page

---
+++ foodbank page/donations_vue.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+                el: '#donationsCard',
+               
+                data: {
+                    items: [],
+                    selectedItem:'',
+                    //this is where you make new variables
+                },
+                mounted: function(){
+                   this.loadItems(); 
+                },
+                methods: {
+                    loadItems: function(){
+                        
+                        // Init variables
+                        var self = this
+                        var app_id = "appQiXNdEEJZqKLF1";
+                        var app_key = "keyHugwZWc2X2ZEpQ";
+                        this.items = []
+                        axios.get(
+                            "https://api.airtable.com/v0/"+app_id+"/Donations?view=Grid%20view",
+                            { 
+                                headers: { Authorization: "Bearer "+app_key } 
+                            }
+                        ).then(function(response){
+                            self.items = response.data.records
+                        }).catch(function(error){
+                            console.log(error)
+                        })      
+        }, 
+                    updateSelectedItem: function(item) {
+                    this.selectedItem = item;
+                    console.log(this.selectedItem);
+                    //return this.selectedItem
+                }
+    }
+});
