0e6126bbd2d5f9c6f35395ba845c69fd9dc8f1e1
mendokusan <41606810+mendokusan@users.noreply.github.com>
2020-11-02 14:06:07 +1300

    Made the login page work finally!

--- foodbank page/scripts/auth.js
+++ foodbank page/scripts/auth.js
@@ -1,3 +1,30 @@
+//listen for auth status Channel
+// auth.onAuthStateChanged(user => {
+//     if (user) {
+//       db.collection('users').onSnapshot(snapshot => {
+//         setupUI(user);
+//       }, err => console.log(err.message));
+//     } else {
+//       setupUI();
+//       console.log("logged out.")
+//     }
+//   });
+
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+    }
+    else {
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block'); 
+    }
+});
+
+
 // logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
@@ -16,10 +43,14 @@ loginForm.addEventListener('submit', (e) => {
     const email = loginForm['login-email'].value;
     const password = loginForm['login-password'].value;
 
+    //log the user in
     auth.signInWithEmailAndPassword(email, password).then(cred => {
         // close the login modal and reset the form
         const modal = document.querySelector('#modal-login');
         M.Modal.getInstance(modal).close();
         loginForm.reset();
     });
+
+
 })
+
--- foodbank page/scripts/index.js
+++ foodbank page/scripts/index.js
@@ -1,3 +1,21 @@
+// const loggedOutLinks = document.querySelectorAll('.logged-out');
+// const loggedInLinks = document.querySelectorAll('.logged-in');
+ 
+
+// const setupUI = (user) => {
+//   if (user) {
+//     // toggle user UI elements
+//     loggedInLinks.forEach(item => item.style.display = 'block');
+//     loggedOutLinks.forEach(item => item.style.display = 'none');
+//   } else {
+//     // clear account info
+//     accountDetails.innerHTML = '';
+//     // toggle user elements
+//     loggedInLinks.forEach(item => item.style.display = 'none');
+//     loggedOutLinks.forEach(item => item.style.display = 'block');
+//   }
+// };
+
 // setup materialize components
 document.addEventListener("DOMContentLoaded", function () {
   var modals = document.querySelectorAll(".modal");
